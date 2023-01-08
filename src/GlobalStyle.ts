ec1395c1542faec57bd9f6ceb172dbd22b9dc7ae
homile <ssanho2@naver.com>
2023-01-08 20:59:42 +0900

    feat: 글로벌 스타일 기능

---
+++ src/GlobalStyle.ts
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+${reset}
+  *{
+    box-sizing: border-box;
+  }
+
+  :root{
+  }
+
+  body{
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    font-family: "NotoSansKR-Regular";
+  }
+  
+  a{
+    text-decoration: none;
+    &:link,&:visited{
+      color: inherit;
+    }
+  }
+  
+  button{
+    cursor: pointer;
+    border: none;
+    padding: 0;
+  }
+`;
+
+export default GlobalStyle;
