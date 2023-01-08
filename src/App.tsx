13498b8771194ba577e4b5b8c4e6e2cc9dbaab09
homile <ssanho2@naver.com>
2023-01-08 20:29:10 +0900

    feat: react-router-dom 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </>
+  );
 };
 
 export default App;
