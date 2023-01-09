d99a0e44d311df623173a0ba2dab73035b6da107
homile <ssanho2@naver.com>
2023-01-09 18:21:01 +0900

    fix: 로그인 시 home으로 이동하지 않던 현상 수정

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -3,13 +3,17 @@ import { auth } from '../../firbaseConfig';
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import styled from 'styled-components';
 import googleLogo from '../../assets/googleLogo.png';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const navigate = useNavigate();
+
   function GoogleLogin() {
     const provider = new GoogleAuthProvider(); // provider를 구글로 설정
     signInWithPopup(auth, provider) // popup을 이용한 signup
       .then((data) => {
         alert('구글 로그인에 성공했습니다.');
+        navigate('/');
       })
       .catch((err) => {
         console.log(err);
